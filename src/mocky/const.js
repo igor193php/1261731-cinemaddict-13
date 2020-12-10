f6befc246bade4224efb4abccc0b96696ae89ffc
igor193php <iigor193@yandex.ru>
2020-12-11 00:11:51 +0300

    refact film moky

--- src/mocky/const.js
+++ src/mocky/const.js
@@ -22,4 +22,112 @@ const NAMES_USER = [
   `Ella`
 ];
 
-export {TYPE_FILM, RANKS_USER, NAMES_USER};
+
+const YEAR_LOW = 1980;
+const YEAR_HIGH = 2010;
+
+const APPROPRIATE_AGE = `18+`;
+
+const NAMES_FILMS = [
+  `Bad Boys for Life`,
+  `Tenet`,
+  `Birds of Prey`,
+  `The Invisible Man`,
+  `Onward`,
+  `Bloodshot`,
+  `Gretel & Hansel`,
+  `The Turning`,
+  `The Hunt`,
+  `Las Pildoras De Mi Novio`
+];
+
+const POSTERS = [
+  `./images/posters/made-for-each-other.png`,
+  `./images/posters/popeye-meets-sinbad.png`,
+  `./images/posters/sagebrush-trail.jpg`,
+  `./images/posters/santa-claus-conquers-the-martians.jpg`,
+  `./images/posters/the-dance-of-life.jpg`,
+  `./images/posters/the-great-flamarion.jpg`,
+  `./images/posters/the-man-with-the-golden-arm.jpg`
+];
+
+const SHOT_DESCRIPTIONS = [
+  `Nulla facilisi etiam dignissim diam quis. Risus ultricies tristique nulla aliquet enim tortor at auctor urna.`,
+  `Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Dolor morbi non arcu risus quis varius quam quisque id. Facilisi morbi tempus iaculis urna id volutpat lacus.`,
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In mollis nunc sed id semper risus in.`,
+  `Cras ornare arcu dui vivamus arcu. Enim eu turpis egestas pretium aenean pharetra magna ac. Imperdiet proin fermentum leo vel orci porta non pulvinar neque.`,
+  `Commodo elit at imperdiet dui accumsan sit amet nulla facilisi. Sit amet risus nullam eget felis eget. Euismod lacinia at quis risus sed vulputate.`
+];
+
+const FULL_DESCRIPTIONS = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+    dolore magna aliqua. Viverra orci sagittis eu volutpat odio facilisis mauris. Dui sapien eget mi proin. 
+    In nibh mauris cursus mattis molestie a iaculis. Felis eget nunc lobortis mattis aliquam faucibus purus.`,
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+    dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Ut sem nulla 
+    pharetra diam sit amet nisl. Congue nisi vitae suscipit tellus mauris. Nunc eget lorem dolor sed viverra.`,
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+    et dolore magna aliqua. Imperdiet massa tincidunt nunc pulvinar sapien et ligula. Quam id leo in vitae 
+    turpis massa. Odio ut enim blandit volutpat maecenas volutpat blandit. Gravida dictum fusce ut placerat
+     orci nulla pellentesque.`,
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+    et dolore magna aliqua. Nunc non blandit massa enim. Tortor condimentum lacinia quis vel eros donec. 
+    Non tellus orci ac auctor augue mauris augue neque. Mi bibendum neque egestas congue quisque egestas diam.`,
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+    dolore magna aliqua. Tristique et egestas quis ipsum suspendisse ultrices gravida. Lacinia at quis risus 
+    sed vulputate. Quis risus sed vulputate odio. Odio pellentesque diam volutpat commodo sed egestas egestas.`
+];
+
+const ACTORS = [
+  `Robert De Niro`,
+  `Jack Nicholson`,
+  `Marlon Brando`,
+  `Denzel Washington`,
+  `Katharine Hepburn`,
+  `Humphrey Bogart`,
+  `Daniel Day-Lewis`,
+  `Sidney Poitier`
+];
+
+const SRCEEN_WRITERS = [
+  `Woody Allen`,
+  `Marshall Brickman`,
+  `Charles Brackett`,
+  `Billy Wilder`,
+  `Paddy Chayefsky`,
+  `Diamond. Based`,
+  `Francis Ford Coppola`,
+  `Mario Puzo`
+];
+
+const DIRECTORS = [
+  `David Lynch`,
+  `Stanley Kubrick`,
+  `Robert Bresson`,
+  `Alfred Hitchcock`,
+  `Martin Scorsese`,
+  `Clint Eastwood`,
+  `William Wyler`
+];
+
+const COUNTRIES = [
+  `USA`,
+  `Europe`
+];
+
+export {
+  TYPE_FILM,
+  RANKS_USER,
+  NAMES_USER,
+  YEAR_LOW,
+  YEAR_HIGH,
+  APPROPRIATE_AGE,
+  NAMES_FILMS,
+  POSTERS,
+  SHOT_DESCRIPTIONS,
+  FULL_DESCRIPTIONS,
+  ACTORS,
+  SRCEEN_WRITERS,
+  DIRECTORS,
+  COUNTRIES
+};
