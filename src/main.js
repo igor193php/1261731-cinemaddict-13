f6e45d37ef0330873c6fbb0438ae0e63d750ad65
igor193php <iigor193@yandex.ru>
2020-12-12 18:49:42 +0300

    step2

--- src/main.js
+++ src/main.js
@@ -4,11 +4,22 @@ import {films, user, filters} from "./engine/data.js";
 
 renderIndex(films, user, filters);
 
-const filmCardElement = document.querySelector(`.film-card__poster`);
+const filmCardElement = document.querySelector(`.film-card`);
+const film = null;
 
 filmCardElement.addEventListener(`click`, (evt) => {
   evt.preventDefault();
+  if (evt.target.matches(`.film-card__poster`)) {
+    const idFilm = +filmCardElement.getAttribute(`id`);
+console.log(typeof idFilm);
+    films.forEach((value) => {
+if (value.id === idFilm)
+        const film = value;
+    });
+
+    renderPopup(film);
+  };
 
 
-  renderPopup(film);
+
 });
